c423ec31bacf4fbf0c321a1cf554279a7a0ca5e3
grayhams74s <felixjunang@gmail.com>
2026-02-05 15:00:25 +0800

    Initial Commit

---
+++ Lists/ListsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListsApp.swift
+//  Lists
+//
+//  Created by felix angcot jr on 2/5/26.
+//
+
+import SwiftUI
+
+@main
+struct ListsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
