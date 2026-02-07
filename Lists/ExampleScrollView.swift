b5b9b06dba9e95aba278315dd82fa6a4b0b33032
grayhams74s <felixjunang@gmail.com>
2026-02-07 18:23:30 +0800

    Commit

---
+++ Lists/ExampleScrollView.swift
@@ -0,0 +1,18 @@
+//
+//  ExampleScrollView.swift
+//  Lists
+//
+//  Created by felix angcot jr on 2/5/26.
+//
+
+import SwiftUI
+
+struct ExampleScrollView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExampleScrollView()
+}
---
+++ Lists/ShoppingModel.swift
@@ -0,0 +1,8 @@
+//
+//  ShoppingModel.swift
+//  Lists
+//
+//  Created by felix angcot jr on 2/5/26.
+//
+
+import Foundation
